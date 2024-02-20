f63dd605d519a8a9dcc0b6eb213255c0a9c8ff33
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-02-21 01:53:17 +0900

    configure project

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "AspectK"
+
