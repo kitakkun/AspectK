089d46f721e3e70d6def095b9335da52bd940e35
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-02-24 12:24:49 +0900

    setup plugin modules

---
+++ aspectk-compiler/build.gradle.kts
@@ -0,0 +1,12 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.ksp)
+}
+
+dependencies {
+    implementation(project(":aspectk-plugin-common"))
+    implementation(libs.kotlin.compiler.embeddable)
+    compileOnly(libs.auto.service)
+    ksp(libs.auto.service.ksp)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,10 @@ dependencyResolutionManagement {
     }
 }
 
+include(":aspectk-compiler")
+include(":aspectk-gradle-plugin")
 include(":aspectk-annotations")
+include(":aspectk-plugin-common")
 
 rootProject.name = "AspectK"
 
