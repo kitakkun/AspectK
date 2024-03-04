d30eac0776e73d36ff7402e4a78abc1719d7595d
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-03-04 21:27:24 +0900

    implement named pointcut token parser

---
+++ aspectk-expression/src/main/kotlin/com/github/kitakkun/aspectk/expression/tokenparser/NamedPointcutTokenParser.kt
@@ -0,0 +1,71 @@
+package com.github.kitakkun.aspectk.expression.tokenparser
+
+import com.github.kitakkun.aspectk.expression.lexer.AspectKToken
+import com.github.kitakkun.aspectk.expression.lexer.AspectKTokenType
+
+data class NamedPointcutToken(
+    val type: NamedPointcutTokenType,
+    val lexeme: String,
+)
+
+enum class NamedPointcutTokenType {
+    PACKAGE,
+    CLASS,
+    FUNCTION,
+}
+
+class NamedPointcutTokenParser(
+    private val rawTokens: List<AspectKToken>,
+) {
+    private val tokens = mutableListOf<NamedPointcutToken>()
+    private var current = 0
+    private val isAtEnd get() = current >= rawTokens.size
+
+    fun parseToNamedPointcutTokens(): List<NamedPointcutToken> {
+        while (!isAtEnd) {
+            scanToken()
+        }
+        return tokens
+    }
+
+    private fun scanToken() {
+        val token = advance()
+
+        when {
+            match(AspectKTokenType.SLASH) -> {
+                addToken(NamedPointcutTokenType.PACKAGE, token.lexeme)
+            }
+
+            match(AspectKTokenType.DOT) -> {
+                addToken(NamedPointcutTokenType.CLASS, token.lexeme)
+            }
+
+            isAtEnd -> {
+                addToken(NamedPointcutTokenType.FUNCTION, token.lexeme)
+            }
+        }
+    }
+
+    private fun advance(): AspectKToken {
+        return rawTokens[current++]
+    }
+
+    private fun match(vararg types: AspectKTokenType): Boolean {
+        for (type in types) {
+            if (check(type)) {
+                advance()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun check(type: AspectKTokenType): Boolean {
+        if (isAtEnd) return false
+        return rawTokens[current].type == type
+    }
+
+    private fun addToken(type: NamedPointcutTokenType, lexeme: String) {
+        tokens.add(NamedPointcutToken(type, lexeme))
+    }
+}
---
+++ aspectk-expression/src/test/kotlin/com/github/kitakkun/aspectk/expression/tokenparser/NamedPointcutTokenParserTest.kt
@@ -0,0 +1,34 @@
+package com.github.kitakkun.aspectk.expression.tokenparser
+
+import com.github.kitakkun.aspectk.expression.lexer.AspectKToken
+import com.github.kitakkun.aspectk.expression.lexer.AspectKTokenType
+import org.junit.Test
+import kotlin.test.assertContentEquals
+
+class NamedPointcutTokenParserTest {
+    @Test
+    fun test() {
+        val rawTokens = listOf(
+            AspectKToken(AspectKTokenType.IDENTIFIER, "com"),
+            AspectKToken(AspectKTokenType.SLASH, "/"),
+            AspectKToken(AspectKTokenType.IDENTIFIER, "example"),
+            AspectKToken(AspectKTokenType.SLASH, "/"),
+            AspectKToken(AspectKTokenType.IDENTIFIER, "ClassA"),
+            AspectKToken(AspectKTokenType.DOT, "."),
+            AspectKToken(AspectKTokenType.IDENTIFIER, "ClassB"),
+            AspectKToken(AspectKTokenType.DOT, "."),
+            AspectKToken(AspectKTokenType.IDENTIFIER, "customPointcut"),
+        )
+        val parser = NamedPointcutTokenParser(rawTokens)
+        val actual = parser.parseToNamedPointcutTokens()
+        val expected = listOf(
+            NamedPointcutToken(NamedPointcutTokenType.PACKAGE, "com"),
+            NamedPointcutToken(NamedPointcutTokenType.PACKAGE, "example"),
+            NamedPointcutToken(NamedPointcutTokenType.CLASS, "ClassA"),
+            NamedPointcutToken(NamedPointcutTokenType.CLASS, "ClassB"),
+            NamedPointcutToken(NamedPointcutTokenType.FUNCTION, "customPointcut"),
+        )
+
+        assertContentEquals(expected = expected, actual = actual)
+    }
+}
